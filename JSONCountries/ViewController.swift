c68aa774a95b934cae60fefada4b865f7dc2aef5
Zaur Giyasov <zagiyas@mera.ru>
2018-02-07 15:17:39 +0300

    first GET request

--- JSONCountries/ViewController.swift
+++ JSONCountries/ViewController.swift
@@ -13,12 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        parseData()
     }
+    
+    func parseData()
+    {
+        let urlString = "https://restcountries.eu/rest/v1/all"
+        guard let url = URL(string: urlString) else {
+            print("*** Can't create URL!")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            if (error != nil) {
+                print(error.debugDescription)
+            } else {
+                
+                do {
+                    let fetchedData = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSArray
+                    print(fetchedData.description)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
